b7b2b4a06be215caa64958161e7432f51d407fd8
Cyanineee-24 <scoutchampion123@gmail.com>
2025-03-30 11:06:38 +0800

    Added Image Slider

--- SERVICES/servicesJS.js
+++ SERVICES/servicesJS.js
@@ -1,7 +1,34 @@
-const menu = document.querySelector('#mobile-menu')
-const menuLinks = document.querySelector('.navbar__menu')
+const sliderTabs = document.querySelectorAll(".slider-tab");
+const sliderIndicator = document.querySelector(".slider-indicator");
 
-menu.addEventListener('click', function() {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.toggle('active');
+// Update the indicator width and position
+const updateIndicator = (tab) => {
+    sliderIndicator.style.left = `${tab.offsetLeft}px`;
+    sliderIndicator.style.width = `${tab.offsetWidth}px`;
+}
+
+// Initialize Swiper instance
+const swiper = new Swiper(".slider-container", {
+    effect: "fade",
+    speed: 1300,
+    autoplay: {delay: 4000},
+    fadeEffect: {
+        crossFade: true // Ensures a clean fade transition
+    },
+    on: {
+        slideChange: () => {
+            updateIndicator(sliderTabs[swiper.activeIndex]);
+        }
+    }
 });
+
+// Update the slide and pagination on tab click
+sliderTabs.forEach((tab, index) => {
+    tab.addEventListener("click", () => {
+        swiper.slideTo(index);
+        updateIndicator(tab);
+    });
+});
+
+updateIndicator(sliderTabs[0]); // Initialize on load
+window.addEventListener("resize", () => updateIndicator(sliderTabs[swiper.activeIndex]));
