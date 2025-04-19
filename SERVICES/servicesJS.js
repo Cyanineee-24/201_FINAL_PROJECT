5d0f067160bf586c5d56650d1555bba37b7e824c
Cyanineee-24 <scoutchampion123@gmail.com>
2025-04-19 23:56:30 +0800

    Linked the navbars in all pages, finalized about us

--- SERVICES/servicesJS.js
+++ SERVICES/servicesJS.js
@@ -3,32 +3,43 @@ const sliderIndicator = document.querySelector(".slider-indicator");
 
 // Update the indicator width and position
 const updateIndicator = (tab) => {
-    sliderIndicator.style.left = `${tab.offsetLeft}px`;
-    sliderIndicator.style.width = `${tab.offsetWidth}px`;
-}
+  sliderIndicator.style.left = `${tab.offsetLeft}px`;
+  sliderIndicator.style.width = `${tab.offsetWidth}px`;
+};
 
 // Initialize Swiper instance
 const swiper = new Swiper(".slider-container", {
-    effect: "fade",
-    speed: 1300,
-    autoplay: {delay: 4000},
-    fadeEffect: {
-        crossFade: true // Ensures a clean fade transition
+  effect: "fade",
+  speed: 1300,
+  autoplay: { delay: 4000 },
+  fadeEffect: {
+    crossFade: true, // Ensures a clean fade transition
+  },
+  on: {
+    slideChange: () => {
+      updateIndicator(sliderTabs[swiper.activeIndex]);
     },
-    on: {
-        slideChange: () => {
-            updateIndicator(sliderTabs[swiper.activeIndex]);
-        }
-    }
+  },
 });
 
 // Update the slide and pagination on tab click
 sliderTabs.forEach((tab, index) => {
-    tab.addEventListener("click", () => {
-        swiper.slideTo(index);
-        updateIndicator(tab);
-    });
+  tab.addEventListener("click", () => {
+    swiper.slideTo(index);
+    updateIndicator(tab);
+  });
 });
 
 updateIndicator(sliderTabs[0]); // Initialize on load
-window.addEventListener("resize", () => updateIndicator(sliderTabs[swiper.activeIndex]));
+window.addEventListener("resize", () =>
+  updateIndicator(sliderTabs[swiper.activeIndex])
+);
+
+// hamburger menu for navbar
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
+
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+});
