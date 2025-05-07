e85ab185440c3e7c488bd185b801ad2373040301
wengGitH <wengiearriola6@gmail.com>
2025-05-07 22:12:30 +0800

    May 7, 2025

--- ABOUT US/aboutUs.js
+++ ABOUT US/aboutUs.js
@@ -6,3 +6,19 @@ menu.addEventListener("click", function () {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
 });
+
+// FOOTER POPUP
+const openBtn = document.getElementById("openPopup");
+const closeBtn = document.getElementById("closePopup");
+const modal = document.getElementById("popup");
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  modal.classList.add("open");
+});
+
+closeBtn.addEventListener("click", () => {
+  modal.classList.remove("open");
+});
+
